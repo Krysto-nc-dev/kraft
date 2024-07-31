66c4be39ba62241d17fd295e1ad09be5b71cd15d
Velten Stoyann <krysto.dev@gmail.com>
2024-07-31 14:43:33 +1100

    initial commit and home page

---
+++ src/lib/constants.ts
@@ -0,0 +1,31 @@
+export const pricingCards = [
+    {
+        title: 'Starter',
+        description: 'Parfait pour essayer Plura',
+        price: 'Gratuit',
+        duration: '',
+        highlight: 'Fonctionnalités clés',
+        features: ['3 sous-comptes', '2 membres de l\'équipe', 'Pipelines illimités'],
+        priceId: '',
+      },
+      {
+        title: 'Illimité',
+        description: 'Le kit ultime pour les agences',
+        price: '3000 XPF',
+        duration: 'mois',
+        highlight: 'Fonctionnalités clés',
+        features: ['Refacturation', 'Équipe de support 24/7'],
+        priceId: 'price_1OYxkqFj9oKEERu1KfJGWxgN',
+      },
+      {
+        title: 'Basique',
+        description: 'Pour les propriétaires d\'agences sérieux',
+        price: '8000 XPF',
+        duration: 'mois',
+        highlight: 'Tout dans Starter, plus',
+        features: ['Sous-comptes illimités', 'Membres de l\'équipe illimités'],
+        priceId: 'price_1OYxkqFj9oKEERu1NbKUxXxN',
+      },
+      
+  ]
+  
